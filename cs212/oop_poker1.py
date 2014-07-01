c3606aeeed6358f8c6cd85c8def7f1d7febc65b9
Charles Lukowski <lukowski.2@gmail.com>
2014-07-01 18:20:36 -0400

    Begin Player class

--- cs212/oop_poker1.py
+++ cs212/oop_poker1.py
@@ -6,7 +6,7 @@ class Table(object):
 	to winning hand.
 	Required: min_bet/max_bet (ie. .50/1.00, 1.00/2.00)
 	"""
-	def __init__(self, min_bet, max_bet, dealer_pos=1, positions=8, state="new")
+	def __init__(self, min_bet, max_bet, dealer_pos=1, positions=8, state="new"):
 		self.min_bet = min_bet
 		self.max_bet = max_bet
 		self.dealer_pos = dealer_pos
@@ -14,4 +14,12 @@ class Table(object):
 		self.state = state
 		self.pot = 0
 
-	
+class Player(object):
+	"""Player (PC/NPC) parent class. Contains properties that are common to 
+	both PC/NPC players. 
+	Required: Name, Starting Funds
+	"""
+	def __init__(self, name, balance=100):
+		self.name = name
+		self.balance = balance
+		
